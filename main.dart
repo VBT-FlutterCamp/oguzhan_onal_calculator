f9a507c9fe788993a7814882c9924993a42f49da
Oğuzhan ÖNAL <oguzhanonal52@hotmail.com>
2022-02-11 22:37:05 +0300

    Null and Empty check added.
    Null and Empty cases clear.

--- main.dart
+++ main.dart
@@ -6,16 +6,27 @@ void main() {
 
   do {
     print("Ilk sayiyi girin:");
-    final double ilkSayi = double.parse(stdin.readLineSync()!);
+    String? birinciSayiString = stdin.readLineSync();
+    while (birinciSayiString == null || birinciSayiString.isEmpty) {
+      print("Geçersiz girdiniz tekrar giriniz");
+      birinciSayiString = stdin.readLineSync();
+    }
+    final double ilkSayi = double.parse(birinciSayiString);
     print("İkinci Sayiyi girin");
-    final double ikinciSayi = double.parse(stdin.readLineSync()!);
+    String? ikinciSayiString = stdin.readLineSync();
+    while (ikinciSayiString == null || ikinciSayiString.isEmpty) {
+      print("Geçersiz girdiniz tekrar giriniz.");
+
+      ikinciSayiString = stdin.readLineSync();
+    }
+    final double ikinciSayi = double.parse(ikinciSayiString);
     final Calculator hesapMakinesi = new Calculator(ilkSayi, ikinciSayi);
     print("Toplama 1");
     print("Çıkarma 2");
     print("Çarpma 3");
     print("Bölme 4");
     print("Çıkış için 4 ten büyük  bir sayi girin");
-    secim = int.parse(stdin.readLineSync()!);
+    secim = int.parse(stdin.readLineSync() ?? "5");
     switch (secim) {
       case 1:
         print("Sonuç ${hesapMakinesi.Sum()}");
